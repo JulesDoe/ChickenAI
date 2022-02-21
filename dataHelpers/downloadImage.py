5eac874d2a03487630c8e8a59a4159262a6fb651
lkaupenj <lukas.kaupenjohann@ise.fraunhofer.de>
2022-02-21 21:36:10 +0100

    only take pictures from 7 to 12am

--- dataHelpers/downloadImage.py
+++ dataHelpers/downloadImage.py
@@ -5,31 +5,39 @@ import logging
 from time import time, sleep
 from datetime import datetime
 
-logging.basicConfig(filename='log.log',
+img_file_path = "/home/pi/training/"
+
+logging.basicConfig(filename='datalog.log',
                     filemode='a',
                     level=logging.INFO,
                     format='%(name)s - %(levelname)s - %(asctime)s - %(message)s')
 
 while True:
-    try:
-        req = requests.get("http://192.168.178.62/snapshot.jpg", timeout=10)
-        img_data = req.content
-        logging.info(f"{req.status_code} {req.elapsed}")
+    if datetime.now().hour >= 7 and datetime.now().hour < 12:
+        try:
+            req = requests.get("http://192.168.178.64/snapshot.jpg", timeout=10)
+            img_data = req.content
+            logging.info(f"{req.status_code} {req.elapsed}")
 
-        # home = os.path.expanduser('~')
-        file = f'D:\chickenAI\{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg'
-        logging.info(file)
-        with open(file, 'wb') as handler:
-            handler.write(img_data)
+            # home = os.path.expanduser('~')
+            file = f'{img_file_path}{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg'
+            logging.info(file)
+            with open(file, 'wb') as handler:
+                handler.write(img_data)
+            
+            #sleep(1.5)
         
-        sleep(1.5)
-    
-    except KeyboardInterrupt:
-        sys.exit()
-    except requests.exceptions.ReadTimeout:
-        logging.error(f"Timeout")
-    except requests.exceptions.ConnectionError:
-        logging.error(f"Timeout")
+        except KeyboardInterrupt:
+            sys.exit()
+        except requests.exceptions.ReadTimeout:
+            logging.error(f"Timeout")
+        except requests.exceptions.ConnectionError:
+            logging.error(f"Timeout")
 
-    except Exception as e:
-        logging.error('%s', exc_info=e)
+        except Exception as e:
+            logging.error('%s', exc_info=e)
+    else:
+        logging.info('wrong time to take a picture')
+    sleep(10)
+
+    
