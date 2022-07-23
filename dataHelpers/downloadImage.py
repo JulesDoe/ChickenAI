3077feddd042815d3b8319277d184b8ed56e0f5c
Jules <example@example.com>
2022-07-23 20:29:10 +0200

    very old coments

--- dataHelpers/downloadImage.py
+++ dataHelpers/downloadImage.py
@@ -17,37 +17,39 @@ logging.basicConfig(filename='datalog.log',
 while True:
     if datetime.now().hour >= 7 and datetime.now().hour < 12:
         try:
-            req = requests.get("http://192.168.178.64/snapshot.jpg", timeout=30)
+            req = requests.get("http://192.168.178.64/snapshot.jpg", timeout=10)
             img_data = req.content
             logging.info(f"Request status:{req.status_code} duration:{req.elapsed} bytes recieved:{len(img_data)}")
 
-            if len(img_data) > 0:
+            if len(img_data) > 0: #saving an image only when actual data comes back
                 file = f'{img_file_path}{datetime.now():%Y-%m-%d-%H-%M-%S}.jpg'
                 logging.info(file)
                 with open(file, 'wb') as handler:
                     handler.write(img_data)
             else:
-                logging.error(f"{len(img_data)} bytes recieved")
-                logging.info("trying to kick ESP cam back into to life")
+                logging.error(f"{len(img_data)} bytes recieved.") # This usualy means the ESP-Cam is available but the webserver doesnt deliver an image.
+                logging.info("Faking a stream user by requesting esp cam front page")
                 req = requests.get("http://192.168.178.64", timeout=10)
-                logging.info(len(req.content))
+                logging.info(f"Request status:{req.status_code} duration:{req.elapsed} bytes recieved:{len(img_data)}")
 
         
         except KeyboardInterrupt:
             sys.exit()
+
         except requests.exceptions.ReadTimeout:
-            logging.error(f"Network Timeout")
+            logging.error(f"Read Timeout")
             logging.info(f"status:{req.status_code} duration:{req.elapsed} bytes recieved:{len(req.content)}")
 
-        except requests.exceptions.ConnectionError:
-            logging.error(f"Network Timeout")
-            logging.info(f"status:{req.status_code} duration:{req.elapsed} bytes recieved:{len(req.content)}")
+        # except requests.exceptions.ConnectionError:
+        #     logging.error(f"Connection Error")
+        #     logging.info(f"status:{req.status_code} duration:{req.elapsed} bytes recieved:{len(req.content)}")
 
         except Exception as e:
             logging.error('%s', exc_info=e)
     else:
         logging.info('wrong time to take a picture; waiting 1h')
         sleep(3600)
+        
     # regular sleep between images
     sleep(10)
 
